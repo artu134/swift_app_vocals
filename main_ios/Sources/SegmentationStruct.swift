c3e7bc70f765d12ba9b5b3b31c57492a7c635824
author <author@gmail.com>
2023-08-21 19:22:08 +0800

    Checked to run

--- main_ios/Sources/SegmentationStruct.swift
+++
@@ -1,7 +0,0 @@
-struct SegmentationResult {
-    // Define the properties of the segmentation result
-    var coordX: float = 0
-    var coordY: float = 0 
-    var coordBottom: float = 0 
-    var coordTop: float = 0
-}
